782cae7fdb20579a446ac494eecfd14c617a158c
Martin Ivanov <martinivanov.it@gmail.com>
2013-05-07 22:15:35 +0300

    Renderer renamed ... again

--- TeamTellurium.Labyrinth/TeamTellurium.Labyrinth/Renderer.cs
+++ TeamTellurium.Labyrinth/TeamTellurium.Labyrinth/Renderer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TeamTellurium.Labyrinth
 {
-    public static class RendererChangedName
+    public static class Renderer
     {
         public static void RenderField(int[,] playField, Position Player)
         {
