ad5a9347196b39ca6f56e30b975e785ca7fd96fa
plamenyovchev <plamenkooo@gmail.com>
2013-05-07 22:06:26 +0300

    deleted test files

--- TeamTellurium.Labyrinth/TeamTellurium.Labyrinth.Tests/ScoreBoardTest.cs
+++ TeamTellurium.Labyrinth/TeamTellurium.Labyrinth.Tests/ScoreBoardTest.cs
@@ -4,10 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TeamTellurium.Labyrinth.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ScoreBoardTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreateScoreboard()
         {
         }
     }
