e250247384ab5352bfda4877df59db784d0a74dd
plamenyovchev <plamenkooo@gmail.com>
2013-05-12 13:05:56 +0300

    scoreboard

--- TeamTellurium.Labyrinth/TeamTellurium.Labyrinth/ScoreBoard.cs
+++ TeamTellurium.Labyrinth/TeamTellurium.Labyrinth/ScoreBoard.cs
@@ -13,38 +13,40 @@ namespace TeamTellurium.Labyrinth
         public string ShowScoreboard()
         {
             StringBuilder scoreboardResult = new StringBuilder();
-            Dictionary<string, string> results = new Dictionary<string, string>();
+            var sortedScoreboard = ReadScoreboardFile();
+            int playerPosition = 0;
+            foreach (var result in sortedScoreboard)
+            {
+                scoreboardResult.AppendFormat("{0}: {1} -> {2}", ++playerPosition, result.Key, result.Value).AppendLine();
+            }
+
+            if (scoreboardResult.ToString() == String.Empty) //if (isEmpty) Console.WriteLine("Scoreboard is empty.");
+            {
+                scoreboardResult.AppendFormat("Scoreboard is empty. Congratulations, you will be the first who will play that game!");
+                //Console.WriteLine("Scoreboard is empty. Congratulations, you will be the first who will play that game!");
+            }
+
+            return scoreboardResult.ToString();
+        }
+
+        private List<KeyValuePair<string, int>> ReadScoreboardFile()
+        {
             FileInfo currentScoreboard = OpenScoreboardFile(); //FileInfo file = OpenFile();
+            var results = new List<KeyValuePair<string, int>>();
 
             using (StreamReader scoreboardList = currentScoreboard.OpenText())
             {
                 string currentLine = null; //string line = null;
-                int playerPosition = 0; //int i = 0;
                 int nameIndex = 0;
                 int scoreIndex = 1;
                 while ((currentLine = scoreboardList.ReadLine()) != null)
                 {
                     string[] nameAndScore = currentLine.Split();
-                    results.Add(nameAndScore[nameIndex],nameAndScore[scoreIndex]);
-                }
-
-                var sortedScoreboard = from result in results
-                                       orderby result.Value, result.Key ascending
-                                       select result;
-                
-                foreach (var result in sortedScoreboard)
-                {                  
-                        scoreboardResult.AppendFormat("{0}: {1} -> {2}", ++playerPosition, result.Key, result.Value).AppendLine();                    
-                }
-
-                if (scoreboardResult.ToString() == String.Empty) //if (isEmpty) Console.WriteLine("Scoreboard is empty.");
-                {
-                    scoreboardResult.AppendFormat("Scoreboard is empty. Congratulations, you will be the first who will play that game!");
-                    //Console.WriteLine("Scoreboard is empty. Congratulations, you will be the first who will play that game!");
+                    results.Add(new KeyValuePair<string, int>(nameAndScore[nameIndex],Int32.Parse(nameAndScore[scoreIndex])));
                 }
             }
-
-            return scoreboardResult.ToString();
+            results.Sort(Compare1);
+            return results;           
         }
 
         private FileInfo OpenScoreboardFile()
@@ -72,17 +74,25 @@ namespace TeamTellurium.Labyrinth
             using (File.Create(filePath)) { }
         }
 
-        public SortedDictionary<string,int> AddPlayerInScoreboard(string playerName, int playerScore) //public void add(string name, int score)
+        public SortedDictionary<string, int> AddPlayerInScoreboard(string playerName, int playerScore) //public void add(string name, int score)
         {
             SortedDictionary<string, int> nameAndScore = new SortedDictionary<string, int>();
-            nameAndScore.Add(playerName, playerScore);
+            if (!string.IsNullOrEmpty(playerName))
+            {
+                nameAndScore.Add(playerName, playerScore);
+            }
             
-            foreach (KeyValuePair<string,int> topScorers in nameAndScore)
+            foreach (KeyValuePair<string, int> topScorers in nameAndScore)
             {
                 File.AppendAllText(SCOREBOARD_PATH, string.Format("{0} {1} {2}", topScorers.Key, topScorers.Value, Environment.NewLine));
             }
 
             return nameAndScore;
+        }
+
+        static int Compare1(KeyValuePair<string, int> a, KeyValuePair<string, int> b)
+        {
+            return a.Value.CompareTo(b.Value);
         }
     }
 }
